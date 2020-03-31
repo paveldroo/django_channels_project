71c5d69e1a75e150c86130954c9eb96d3fa1dafe
Pavel Druzhinin <p.druzhinin@corp.mail.ru>
2020-03-31 13:47:50 +0300

    accept websocket connect and send message

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -8,6 +8,14 @@ from channels.db import database_sync_to_async
 class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         print('connected', event)
+        await self.send({
+            'type': 'websocket.accept'
+        })
+        # await asyncio.sleep(10)
+        await self.send({
+            'type': 'websocket.send',
+            'text': 'Hello World!'
+        })
 
     async def websocket_receive(self, event):
         print('receive', event)
